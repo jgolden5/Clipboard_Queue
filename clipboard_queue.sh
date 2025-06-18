35ac4defe7af880460f7ae5ba7093ec928f53454
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-06-18 14:17:34 -0700

    allow a file to be passed into cq and read one line at a time

--- clipboard_queue.sh
+++ clipboard_queue.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 clipboard_queue() {
   local selection_index=1;
-  while (( selection_index <= "$#" )); do
+  local lines=
+  local number_of_lines=
+  if [[ $# == 1 && -f $1 ]]; then
+    lines=$(cat $1)
+    number_of_lines=$(wc -l $1 | awk '{ print $1 }' | sed 's/ //g')
+  else
+    lines=$@
+    number_of_lines=$#
+  fi
+  while (( selection_index <= "$number_of_lines" )); do
     i=1
     clear
-    for param in "$@"; do
-      if [[ $i == $selection_index ]]; then
-        echo ">> $param"
-        echo "$param" | pbcopy
-      else
-        echo "$param"
-      fi
-      ((i++))
-    done
+    print_lines "${lines[@]}"
     read -n1 -s -p "Hit 'n' for next, 'p' for previous, or 'q' for quit" user_selection
     case $user_selection in
       n)
@@ -34,6 +35,19 @@ clipboard_queue() {
         sleep 0.5
         ;;
     esac
+  done
+  echo
+}
+
+print_lines() {
+  for line in ${lines[@]}; do
+    if [[ $i == $selection_index ]]; then
+      echo ">> $line"
+      echo "$line" | pbcopy
+    else
+      echo "$line"
+    fi
+    ((i++))
   done
 }
 
